d06facbc822cb5d62629d4fc932298bc487f89f6
Patrick Lang <plang@microsoft.com>
2019-11-07 15:55:13 -0800

    Fix build

--- workshop/vagrant/gitk8s.sh
+++ workshop/vagrant/gitk8s.sh
@@ -1,3 +1,5 @@
 mkdir -p ~/go/src/k8s.io/
 cd ~/go/src/k8s.io
-git clone --depth 1 https://github.com/kubernetes/kubernetes.git
+# Pull just a single branch and no history to save space
+# The Kubernetes build/release script needs a tag to determine version, so --branch release-1.16 won't work
+git clone --branch v1.16.3-beta.0 https://github.com/kubernetes/kubernetes.git --depth 2
